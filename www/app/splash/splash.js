f6071bb80956608f07f5ac8c1a085303ee9ab1a5
Carlos Canizal <carlos@codecery.com>
2014-05-25 16:36:39 -0500

    Adds User login

--- www/app/splash/splash.js
+++ www/app/splash/splash.js
@@ -4,12 +4,11 @@ angular.module('app.controllers')
   .controller('SplashCtrl', ['$scope', function($scope) {
     
   }])
-  .controller('LoginCtrl', ['$scope', function($scope) {
-    
-  }])
-  .controller('RegisterCtrl', ['$scope', function($scope) {
+  .controller('LogInCtrl', ['$scope', function($scope) {
     $scope.user = {};
-    
+  }])
+  .controller('SignUpCtrl', ['$scope', function($scope) {
+    $scope.user = {};
   }])
   .controller('SignUpFormCtrl', ['$scope', 'Auth', function($scope, Auth) {
     
@@ -19,6 +18,37 @@ angular.module('app.controllers')
       $scope.response.error = false;
       if($scope.signUpForm.$valid){
         Auth.signUp($scope.user).then(
+          function(user){
+            console.log(user);
+          },function(error){
+            console.log(error);
+            $scope.response.error = true;
+            $scope.response.message = error.message;
+          }
+        );
+      }
+    };
+
+    $scope.getCssClasses = function(ngModelController) {
+      return {
+        error: ngModelController.$invalid && ngModelController.$dirty,
+        success: ngModelController.$valid && ngModelController.$dirty
+      };
+    };
+
+    $scope.showError = function(ngModelController, error) {
+      return ngModelController.$error[error] && ngModelController.$dirty ;
+    };
+    
+  }])
+  .controller('LogInFormCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    
+    $scope.response = {error:false, message:""};
+
+    $scope.logIn = function(){
+      $scope.response.error = false;
+      if($scope.logInForm.$valid){
+        Auth.logIn($scope.user).then(
           function(user){
             console.log(user);
           },function(error){
--- www/common/services/auth/auth.js
+++ www/common/services/auth/auth.js
@@ -12,6 +12,16 @@ app
       });
 
       return deferred.promise;
+    },
+    logIn : function(params){
+      var deferred = $q.defer();
+      Parse.User.logIn(params.username, params.password)
+      .then(function(user){
+        deferred.resolve(user);
+      },function(error){
+        deferred.reject(error);
+      });
+      return deferred.promise;
     }
   };
 
