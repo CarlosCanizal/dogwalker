697c0303359bfd65ccd4ca9204eff979d2e3819e
Carlos Canizal <carlos@codecery.com>
2014-05-27 09:57:11 -0500

    Integrates Cordova Facebook Plugin

--- platforms/ios/www/app/splash/splash.js
+++ platforms/ios/www/app/splash/splash.js
@@ -1,6 +1,84 @@
 'use strict';
 
 angular.module('app.controllers')
-  .controller('SplashCtrl', ['$scope', '$state', function($scope, $state) {
+  .controller('SplashCtrl', ['$scope','Facebook', function($scope, Facebook) {
+    // Facebook.loadSDK();
+
+  }])
+  .controller('LogInCtrl', ['$scope', function($scope) {
+    $scope.user = {};
+  }])
+  .controller('SignUpCtrl', ['$scope', function($scope) {
+    $scope.user = {};
+  }])
+  .controller('SignUpFormCtrl', ['$scope', 'Auth', function($scope, Auth) {
+    
+    $scope.response = {error:false, message:""};
+
+    $scope.signUp = function(){
+      var ref = window.open('http://localhost', '_blank', 'location=yes');
+    alert();
+      $scope.response.error = false;
+      if($scope.signUpForm.$valid){
+        Auth.signUp($scope.user).then(
+          function(user){
+            console.log(user);
+          },function(error){
+            console.log(error);
+            $scope.response.error = true;
+            $scope.response.message = error.message;
+          }
+        );
+      }
+    };
+
+    $scope.getCssClasses = function(ngModelController) {
+      return {
+        error: ngModelController.$invalid && ngModelController.$dirty,
+        success: ngModelController.$valid && ngModelController.$dirty
+      };
+    };
+
+    $scope.showError = function(ngModelController, error) {
+      return ngModelController.$error[error] && ngModelController.$dirty ;
+    };
+    
+  }])
+  .controller('LogInFormCtrl', ['$scope', 'Auth','Facebook', function($scope, Auth, Facebook) {
+    
+    $scope.response = {error:false, message:""};
+
+    $scope.logIn = function(){
+      $scope.response.error = false;
+      if($scope.logInForm.$valid){
+        Auth.logIn($scope.user).then(
+          function(user){
+            console.log(user);
+          },function(error){
+            console.log(error);
+            $scope.response.error = true;
+            $scope.response.message = error.message;
+          }
+        );
+      }
+    };
+
+    $scope.facebookLogIn = function(){
+      Facebook.logIn().then(function(token){
+        alert(token);
+        $scope.facebook = token;
+      });
+    };
+
+    $scope.getCssClasses = function(ngModelController) {
+      return {
+        error: ngModelController.$invalid && ngModelController.$dirty,
+        success: ngModelController.$valid && ngModelController.$dirty
+      };
+    };
+
+    $scope.showError = function(ngModelController, error) {
+      return ngModelController.$error[error] && ngModelController.$dirty ;
+    };
     
   }]);
